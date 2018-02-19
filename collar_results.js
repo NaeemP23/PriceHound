e672c56313388f5ef248cf6f642dabbaab650478
Nancy Yao <nancy.yao2@gmail.com>
2018-02-18 22:13:30 -0600

    search bar on results page; filters for show

---
+++ collar_results.js
@@ -0,0 +1,7 @@
+function filterColor() {
+	document.getElementById("filter-color").classList.toggle("show");
+}
+
+function filterPrice() {
+	document.getElementById("filter-price").classList.toggle("show");
+}
