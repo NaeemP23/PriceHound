f62a2e5879c5603e975dfcc46826d7f15ad170d7
rikako102 <rika.ko102@gmail.com>
2018-02-24 11:20:55 -0600

    graph works now wooo

--- collar_product.js
+++ collar_product.js
@@ -1,55 +1,56 @@
-var ctx = document.getElementById("priceGraph");
-Chart.defaults.global.defaultFontColor = 'rgba(52,54,53,1)';
-var priceGraph = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: ["Aug 2017","Sep 2017","Oct 2017","Nov 2017","Dec 2017","Jan 2018","Feb 2018"],
-        datasets: [{
-            label: 'Price',
-            data: [14,17,18,18,20,17,16],
-            fill: false,
-            borderColor: [
-                'rgba(255,255,255,1.0)',
-            ],
-            borderWidth: 2,
-            lineTension: 0,
-            hoverRadius: 8,
-        },
-        {
-            label: 'Average',
-            data: [17,17,17,17,17,17,17],
-            fill: false,
-            borderColor: [
-                'rgba(255,140,0,1.0)',
-            ],
-            borderWidth: 2,
-            lineTension: 0,
-        }]
+var time = ["Aug 2017","Sep 2017","Oct 2017","Nov 2017","Dec 2017","Jan 2018","Feb 2018"];
+var config = {
+  type: 'line',
+  data: {
+    labels: time,
+    datasets: [{
+      label: "Price",
+      borderColor: 'rgba(211,211,211,1)',
+      backgroundColor: 'rgba(211,211,211,1)',
+      data: [14,17,18,18,20,17,16],
+      fill: false,
+      lineTension: 0,
+    }, {
+      label: "Average",
+      borderColor: 'rgba(210, 82, 47, 1)',
+      backgroundColor: 'rgba(210, 82, 47, 1)',
+      data: [17,17,17,17,17,17,17],
+      fill: false,
+    }]
+  },
+  options: {
+    responsive: true,
+    title:{
+      display:true,
+      text:'Price History'
     },
-    options: {
-        responsive: true,
-        title:{
-            display:true,
-            text:'Price History'
-        },
-        tooltips: {
-            // mode: 'index',
-            // intersect: false,
-        },
-        hover: {
-            // mode: 'nearest',
-            // intersect: true,
-        },
-        scales: {
-            yAxes: [{
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Price (USD)',
-                  },
-                }]
-            }
-        },
-        onHover: function(event,elements) {
-             // $("#canvas1").css("cursor", elements[0] ? "pointer" : "default");
+    tooltips: {
+      mode: 'index',
+      intersect: false,
+    },
+   hover: {
+      mode: 'nearest',
+      intersect: true
+    },
+    scales: {
+      xAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Month'
         }
-});
+      }],
+      yAxes: [{
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Price (USD)'
+        },
+      }]
+    }
+  }
+};
+
+var ctx = document.getElementById("priceGraph").getContext("2d");
+var myLine = new Chart(ctx, config);
+
