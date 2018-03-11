cdbc94a8a9cdcec652e412dedb004db0c183fa59
naeempatel23 <35852897+naeempatel23@users.noreply.github.com>
2018-03-11 00:25:10 -0600

    Add files via upload

--- account.js
+++ account.js
@@ -1,4 +1,22 @@
 
-function clickMe(){
+function clickMe()
+{
     alert("Success-- Information Changed.");
+};
+
+function buttons(evt, bttnName)
+{
+    var i, button_content, button_info;
+    button_content = document.getElementsByClassName("button_content");
+    for (i = 0; i < button_content.length; i++) {
+        button_content[i].style.display = "none";
+    }
+    button_info = document.getElementsByClassName("button_info");
+    for (i = 0; i < button_info.length; i++) {
+        button_info[i].className = button_info[i].className.replace(" active", "");
+    }
+    document.getElementById(bttnName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
+
+document.getElementById("defaultOpen").click();
